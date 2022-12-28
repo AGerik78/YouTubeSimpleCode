b10a92bec463ed31fee86a4b73218e8c2fe786ff
Alexey <gercoo@mail.ru>
2022-12-28 14:25:19 +0300

    homeWork04_if_else

---
+++ HomeWork04_if_else/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Homework
+{
+    // 1. Напишите простой калькулятор с помощью if_else.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                System.Console.Clear();
+                double firstValue, secondValue;
+                string action;
+
+                try
+                {
+                    System.Console.WriteLine("Введите число 1");
+                    firstValue = double.Parse(System.Console.ReadLine()!);
+
+                    System.Console.WriteLine("Введите число 2");
+                    secondValue = double.Parse(System.Console.ReadLine()!);
+                }
+                catch (Exception)
+                {
+                    System.Console.WriteLine("Ошибка ввода числа!");
+                    System.Console.ReadLine();
+                    continue;
+                }
+
+                System.Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+                action = System.Console.ReadLine();
+
+                if (action == "+")
+                {
+                    System.Console.WriteLine(firstValue + secondValue);
+                }
+                else if (action == "-")
+                {
+                    System.Console.WriteLine(firstValue - secondValue);
+                }
+                else if (action == "*")
+                {
+                    System.Console.WriteLine(firstValue * secondValue);
+                }
+                else if (action == "/")
+                {
+                    if (secondValue == 0)
+                    {
+                        System.Console.WriteLine(0); // если одна строка кода то можно фигурные скобки {} убрать.
+                    }
+                    else
+                    {
+                        System.Console.WriteLine(firstValue / secondValue);
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("Ошибка! Неизвестное действие!");
+                }
+
+                System.Console.ReadLine();
+
+            }
+
+        }
+    }
+}
+// как выйти из программы?
